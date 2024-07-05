b11feb7917ded61664787f7199ab0fc55eb36b08
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-07-05 11:25:01 -0600

    __init__.py update
    change bd name  create dogs and blogs table

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,24 @@
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
+CONN = sqlite3.connect('dog_blogs.db')
 CURSOR = CONN.cursor()
+
+# create dogs table
+CURSOR.execute("""
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      blog_name TEXT NOT NULL,
+      dog_id INTEGER,
+      FOREIGN KEY(blog_id)  REFERENCES blogs(id)
+
+    )
+""")
+
+# create blogs table
+CURSOR.execute("""
+    CREATE TABLE IF NOT EXISTS blogs (
+        id INTEGER PRIMARY KEY, blog_name TEXT NOT NULL,
+        dog_id INTEGER,
+        FOREIGN KEY(dog_id) REFERENCES dogs(id)
+    )
+""")
